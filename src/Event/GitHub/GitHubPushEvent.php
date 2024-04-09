2aaafb0aec47546c48ac98e0290eedabfd6f7b76
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 09:14:07 -0500

    Update GitHubPushEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/GitHub/GitHubPushEvent.php
+++ src/Event/GitHub/GitHubPushEvent.php
@@ -19,8 +19,7 @@ final class GitHubPushEvent implements GitHubEventInterface
 
     public function __construct(
         private string $content
-    ) {
-    }
+    ) {}
 
     public function payload(): string
     {
