ebb08bf90d0c1659b04cf41f3f93e0805b4ae361
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-28 14:56:58 -0500

    Update AbstractTool.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/AbstractTool.php
+++ src/Tool/AbstractTool.php
@@ -37,8 +37,6 @@ abstract class AbstractTool implements ToolInterface
 
     public function phive(string $executable): string
     {
-        return $executable;
-
         return dirname(__FILE__, 3) . '/tools/' . $executable;
     }
 
