827a282812ac81adf45c56ee7008c430190cc6ea
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-22 07:20:51 -0500

    Update AbstractCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -8,9 +8,9 @@ use Ghostwriter\Compliance\Compliance;
 use Ghostwriter\Compliance\Event\OutputEvent;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\EventDispatcher\Interface\EventDispatcherInterface;
-use Ghostwriter\EventDispatcher\Interface\EventInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Throwable;
 
 use function mb_strtolower;
 use function sprintf;
@@ -27,28 +27,42 @@ abstract class AbstractCommand extends Command
     }
 
     /**
-     * @param class-string<EventInterface<bool>> $event
+     * @param class-string $event
      *
      * @return int 0 if everything went fine, or an exit code
      */
     public function dispatch(string $event): int
     {
-        return $this->dispatcher->dispatch(
-            $this->container->build($event)
-        )->isPropagationStopped() ? self::FAILURE : self::SUCCESS;
+        try {
+            $this->dispatcher->dispatch($this->container->build($event));
+        } catch (Throwable $e) {
+            $this->symfonyStyle->error($e->getMessage());
+
+            return self::FAILURE;
+        }
+
+        return self::SUCCESS;
     }
 
     public function write(string $message): int
     {
-        return $this->dispatcher->dispatch(
-            new OutputEvent([
-                '::echo::on',
-                sprintf('::group::%s %s', Compliance::NAME, Compliance::BLACK_LIVES_MATTER),
-                $message,
-                '::endgroup::',
-                '::echo::off',
-            ])
-        )->isPropagationStopped() ? self::FAILURE : self::SUCCESS;
+        try {
+            $this->dispatcher->dispatch(
+                new OutputEvent([
+                    '::echo::on',
+                    sprintf('::group::%s %s', Compliance::NAME, Compliance::BLACK_LIVES_MATTER),
+                    $message,
+                    '::endgroup::',
+                    '::echo::off',
+                ])
+            );
+        } catch (Throwable $e) {
+            $this->symfonyStyle->error($e->getMessage());
+
+            return self::FAILURE;
+        }
+
+        return self::SUCCESS;
     }
 
     public static function getDefaultName(): string
