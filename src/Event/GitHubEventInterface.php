d442c630966938170fb83c93e07a2c59ac56d2f8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-22 07:20:46 -0500

    Update GitHubEventInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/GitHubEventInterface.php
+++ src/Event/GitHubEventInterface.php
@@ -4,14 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Event;
 
-use Ghostwriter\EventDispatcher\Interface\EventInterface;
-
-/**
- * @template T of bool
- *
- * @extends EventInterface<T>
- */
-interface GitHubEventInterface extends EventInterface
+interface GitHubEventInterface
 {
     public function payload(): string;
 }
