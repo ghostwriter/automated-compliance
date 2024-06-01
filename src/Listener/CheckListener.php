995828c6ff9df8ff95c865a0c941fd714f42eb34
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 10:11:45 -0500

    Update CheckListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/CheckListener.php
+++ src/Listener/CheckListener.php
@@ -7,15 +7,19 @@ namespace Ghostwriter\Compliance\Listener;
 use Ghostwriter\Compliance\Event\CheckEvent;
 use Ghostwriter\Compliance\Event\OutputEvent;
 use Ghostwriter\Compliance\Interface\EventListenerInterface;
+use Throwable;
 
 final readonly class CheckListener implements EventListenerInterface
 {
+    /**
+     * @throws Throwable
+     */
     public function __invoke(CheckEvent $checkEvent): void
     {
         /** @var string $job */
         $job = $checkEvent->input()
             ->getArgument('job');
 
-        $checkEvent->dispatch(new OutputEvent($job));
+        $checkEvent->dispatch(OutputEvent::new($job));
     }
 }
