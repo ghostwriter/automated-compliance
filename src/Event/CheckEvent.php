15fe31fe13024382d2f55f6fcd7a901556470183
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:28 -0500

    Update CheckEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/CheckEvent.php
+++ src/Event/CheckEvent.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Event;
 
-final readonly class CheckEvent extends AbstractEvent {}
+final readonly class CheckEvent extends AbstractEvent
+{
+}
