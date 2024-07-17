48638f596e1ef586ed998debd44f33d18a345af0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:27 -0500

    Update PHPUnit.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/PHPUnit.php
+++ src/Tool/PHPUnit.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Tool;
 
+use Override;
+
 final class PHPUnit extends AbstractTool
 {
+    #[Override]
     public function command(): string
     {
         return 'composer ghostwriter:phpunit:test';
@@ -14,6 +17,7 @@ final class PHPUnit extends AbstractTool
     /**
      * @return string[]
      */
+    #[Override]
     public function configuration(): array
     {
         return ['phpunit.xml.dist', 'phpunit.xml'];
