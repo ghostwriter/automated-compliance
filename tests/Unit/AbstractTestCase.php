cf82f1a40397ed9387927f26eafc3e83e14831cd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:26 -0500

    Update AbstractTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/AbstractTestCase.php
+++ tests/Unit/AbstractTestCase.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use Override;
 use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTestCase extends TestCase
 {
+    #[Override]
     protected function setUp(): void
     {
         parent::setUp();
