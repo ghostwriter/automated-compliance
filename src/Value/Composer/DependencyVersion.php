a60f94fba70f605970f7aaf0022402f219c370cd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:26 -0500

    Add DependencyVersion.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Composer/DependencyVersion.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Value\Composer;
+
+use InvalidArgumentException;
+use JsonSerializable;
+use Override;
+use Stringable;
+
+use function trim;
+
+final readonly class DependencyVersion implements JsonSerializable, Stringable
+{
+    public function __construct(
+        private string $content
+    ) {
+        if (trim($content) === '') {
+            throw new InvalidArgumentException('Version cannot be empty');
+        }
+    }
+
+    #[Override]
+    public function __toString(): string
+    {
+        return $this->content;
+    }
+
+    #[Override]
+    public function jsonSerialize(): array
+    {
+        return [$this->content];
+    }
+
+    public static function new(string $content): self
+    {
+        return new self($content);
+    }
+}
