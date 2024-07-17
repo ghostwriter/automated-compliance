1aed7c3ebf42c788451fbd962b7d8d3d24f8e579
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:29 -0500

    Update AbstractCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Command;
 
-use Ghostwriter\Compliance\Compliance;
-use Ghostwriter\Compliance\Event\OutputEvent;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\EventDispatcher\Interface\EventDispatcherInterface;
+use Override;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Throwable;
+
+use const PHP_EOL;
 
 use function mb_strtolower;
 use function sprintf;
@@ -20,7 +21,7 @@ abstract class AbstractCommand extends Command
 {
     public function __construct(
         protected ContainerInterface $container,
-        protected EventDispatcherInterface $dispatcher,
+        protected EventDispatcherInterface $eventDispatcher,
         protected SymfonyStyle $symfonyStyle
     ) {
         parent::__construct(static::getDefaultName());
@@ -34,30 +35,17 @@ abstract class AbstractCommand extends Command
     public function dispatch(string $event): int
     {
         try {
-            $this->dispatcher->dispatch($this->container->build($event));
-        } catch (Throwable $e) {
-            $this->symfonyStyle->error($e->getMessage());
-
-            return self::FAILURE;
-        }
-
-        return self::SUCCESS;
-    }
-
-    public function write(string $message): int
-    {
-        try {
-            $this->dispatcher->dispatch(
-                OutputEvent::new([
-                    '::echo::on',
-                    sprintf('::group::%s %s', Compliance::NAME, Compliance::BLACK_LIVES_MATTER),
-                    $message,
-                    '::endgroup::',
-                    '::echo::off',
-                ])
+            $this->eventDispatcher->dispatch($this->container->build($event));
+        } catch (Throwable $throwable) {
+            $this->symfonyStyle->error(
+                sprintf(
+                    '[%s] %s%s%s' . PHP_EOL,
+                    $throwable::class,
+                    $throwable->getMessage(),
+                    PHP_EOL . PHP_EOL,
+                    $throwable->getTraceAsString(),
+                )
             );
-        } catch (Throwable $e) {
-            $this->symfonyStyle->error($e->getMessage());
 
             return self::FAILURE;
         }
@@ -65,6 +53,36 @@ abstract class AbstractCommand extends Command
         return self::SUCCESS;
     }
 
+    //    public function write(string $message): int
+    //    {
+    //        try {
+    //            $this->eventDispatcher->dispatch(
+    //                OutputEvent::new([
+    //                    '::echo::on',
+    //                    sprintf('::group::%s %s', Compliance::NAME, Compliance::BLACK_LIVES_MATTER),
+    //                    $message,
+    //                    '::endgroup::',
+    //                    '::echo::off',
+    //                ])
+    //            );
+    //        } catch (Throwable $throwable) {
+    //            $this->symfonyStyle->error(
+    //                sprintf(
+    //                    '[%s] %s%s%s' . PHP_EOL,
+    //                    $throwable::class,
+    //                    $throwable->getMessage(),
+    //                    PHP_EOL . PHP_EOL,
+    //                    $throwable->getTraceAsString(),
+    //                )
+    //            );
+    //
+    //            return self::FAILURE;
+    //        }
+    //
+    //        return self::SUCCESS;
+    //    }
+
+    #[Override]
     public static function getDefaultName(): string
     {
         return mb_strtolower(str_replace([__NAMESPACE__ . '\\', 'Command'], '', static::class));
