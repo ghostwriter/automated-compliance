691e648ecee2da309a360812405d4b1e5d608d90
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:26 -0500

    Add License.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Composer/License.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Value\Composer;
+
+use InvalidArgumentException;
+use JsonSerializable;
+use Override;
+use Stringable;
+
+use function trim;
+
+final readonly class License implements JsonSerializable, Stringable
+{
+    public function __construct(
+        private string $content
+    ) {
+    }
+
+    #[Override]
+    public function __toString(): string
+    {
+        return $this->content;
+    }
+
+    #[Override]
+    public function jsonSerialize(): array
+    {
+        return [$this->content];
+    }
+
+    public static function new(?string $content): self
+    {
+        return match (true) {
+            $content === null => throw new InvalidArgumentException('License cannot be null'),
+            trim($content) === '' => throw new InvalidArgumentException('License cannot be empty'),
+            default => new self($content)
+        };
+    }
+}
