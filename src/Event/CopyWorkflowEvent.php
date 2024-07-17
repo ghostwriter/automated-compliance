4288268c4f8b61e05d67b1b7ee577679d2d69ac2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:32 -0500

    Add CopyWorkflowEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Event/CopyWorkflowEvent.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Event;
+
+final readonly class CopyWorkflowEvent
+{
+    private const string WORKFLOW_FILE = __DIR__ . '/../../automation.yml.dist';
+
+    public function __construct(
+        private string $to,
+        private bool $overwrite,
+    ) {
+    }
+
+    public function from(): string
+    {
+        return self::WORKFLOW_FILE;
+    }
+
+    public function overwrite(): bool
+    {
+        return $this->overwrite;
+    }
+
+    public function to(): string
+    {
+        return $this->to;
+    }
+
+    public static function new(string $to, bool $overwrite = false): self
+    {
+        return new self($to, $overwrite);
+    }
+}
