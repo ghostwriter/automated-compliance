fdc35922397c4936c945bac5444526b7b4d0be85
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:32 -0500

    Add FailedToFindExecutableException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/FailedToFindExecutableException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Exception;
+
+use RuntimeException;
+
+final class FailedToFindExecutableException extends RuntimeException
+{
+}
