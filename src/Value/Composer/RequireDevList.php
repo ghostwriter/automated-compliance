c337baf83ff553585cd2b0afe4bb97ae66212e4b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:26 -0500

    Add RequireDevList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Composer/RequireDevList.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Value\Composer;
+
+use Generator;
+use IteratorAggregate;
+use Override;
+
+/**
+ * @implements IteratorAggregate<Package|Extension>
+ */
+final readonly class RequireDevList implements IteratorAggregate
+{
+    /**
+     * @param array<Extension|Package> $requireDevList
+     */
+    public function __construct(
+        private array $requireDevList,
+    ) {
+    }
+
+    #[Override]
+    public function getIterator(): Generator
+    {
+        yield from $this->requireDevList;
+    }
+
+    public static function new(array $requireDev): self
+    {
+        $requireDevList = [];
+
+        foreach ($requireDev as $name => $version) {
+            $dependencyName = DependencyName::new($name);
+            $dependencyVersion = DependencyVersion::new($version);
+
+            $requireDevList[$name] = $dependencyName->isPhpExtension()
+                ? Extension::new($dependencyName, $dependencyVersion)
+                : Package::new($dependencyName, $dependencyVersion);
+        }
+
+        return new self($requireDevList);
+    }
+}
