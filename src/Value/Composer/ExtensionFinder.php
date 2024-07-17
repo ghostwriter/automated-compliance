49076933f0cc3b1d54e4a4b3ffd38cf691487202
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:26 -0500

    Add ExtensionFinder.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Composer/ExtensionFinder.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Value\Composer;
+
+final readonly class ExtensionFinder
+{
+    public function __construct(
+        private Composer $composer,
+    ) {
+    }
+
+    public function find(string $composerJsonPath): Extensions
+    {
+        $extensions = [];
+
+        $composerJson = $this->composer->readJsonFile($composerJsonPath);
+
+        foreach ($composerJson->getRequire() as $requireDevList) {
+            if (! $requireDevList instanceof Extension) {
+                continue;
+            }
+
+            $extensions[] = $requireDevList;
+        }
+
+        foreach ($composerJson->getRequireDev() as $requireDevList) {
+            if (! $requireDevList instanceof Extension) {
+                continue;
+            }
+
+            $extensions[] = $requireDevList;
+        }
+
+        return new Extensions($extensions);
+    }
+}
