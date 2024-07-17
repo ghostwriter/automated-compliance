eff113b05ea2662fe8bed19863f32b0468dd2b53
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 10:04:26 -0500

    Add OutputListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Event/Listener/OutputListener.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Event\Listener;
+
+use Ghostwriter\Compliance\Event\OutputEvent;
+use Ghostwriter\Compliance\Interface\Event\Listener\ListenerInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final readonly class OutputListener implements ListenerInterface
+{
+    public function __construct(
+        private SymfonyStyle $symfonyStyle,
+    ) {
+    }
+
+    public function __invoke(OutputEvent $outputEvent): void
+    {
+        $this->symfonyStyle->writeln($outputEvent->getMessage());
+    }
+}
