b57a7a60e1f2a7bbf13167e5ecd00ddec6ebaf59
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:26 -0500

    Add Package.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Composer/Package.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Value\Composer;
+
+use Ghostwriter\Compliance\Interface\Composer\DependencyInterface;
+use Ghostwriter\Json\Json;
+use Override;
+
+final readonly class Package implements DependencyInterface
+{
+    public function __construct(
+        private DependencyName $dependencyName,
+        private DependencyVersion $dependencyVersion
+    ) {
+    }
+
+    #[Override]
+    public function __toString(): string
+    {
+        return (new Json())->encode($this);
+    }
+
+    #[Override]
+    public function jsonSerialize(): array
+    {
+        return [
+            $this->dependencyName->__toString() => $this->dependencyVersion->__toString(),
+        ];
+    }
+
+    #[Override]
+    public function name(): DependencyName
+    {
+        return $this->dependencyName;
+    }
+
+    #[Override]
+    public function version(): DependencyVersion
+    {
+        return $this->dependencyVersion;
+    }
+
+    public static function new(DependencyName $dependencyName, DependencyVersion $dependencyVersion): self
+    {
+        return new self($dependencyName, $dependencyVersion);
+    }
+}
