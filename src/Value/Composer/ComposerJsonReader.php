236ee23cc571ebe9cf192aa67ceb48e09cf01685
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:26 -0500

    Add ComposerJsonReader.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Composer/ComposerJsonReader.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Value\Composer;
+
+use Ghostwriter\Json\Json;
+use InvalidArgumentException;
+
+use function file_exists;
+use function file_get_contents;
+
+final readonly class ComposerJsonReader
+{
+    public function read(string $composerJsonPath): ComposerJson
+    {
+        if (! file_exists($composerJsonPath)) {
+            throw new InvalidArgumentException('Composer JSON file does not exist: ' . $composerJsonPath);
+        }
+
+        $composerJsonContents = file_get_contents($composerJsonPath);
+
+        if ($composerJsonContents === false) {
+            throw new InvalidArgumentException('Composer JSON file could not be read');
+        }
+
+        return new ComposerJson($composerJsonPath, (new Json())->decode($composerJsonContents));
+    }
+}
