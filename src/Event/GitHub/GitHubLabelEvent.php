8675926e2c85ade0b526e7217cc2b70cd0626c2e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:28 -0500

    Update GitHubLabelEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/GitHub/GitHubLabelEvent.php
+++ src/Event/GitHub/GitHubLabelEvent.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Event\GitHub;
 
-use Ghostwriter\Compliance\Event\GitHubEventInterface;
+use Ghostwriter\Compliance\Interface\Event\GitHubEventInterface;
+use Override;
 
 /**
  * @implements GitHubEventInterface<bool>
  */
-final class GitHubLabelEvent implements GitHubEventInterface
+final readonly class GitHubLabelEvent implements GitHubEventInterface
 {
     public function __construct(
         private string $content
-    ) {}
+    ) {
+    }
 
+    #[Override]
     public function payload(): string
     {
         return $this->content;
