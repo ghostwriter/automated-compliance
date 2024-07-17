307fa64c0010e87f84cf7462601a3122ee3d1fef
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:27 -0500

    Update GitHubListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/GitHubListener.php
+++ src/Listener/GitHubListener.php
@@ -4,21 +4,26 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Listener;
 
-use Ghostwriter\Compliance\Event\GitHubEventInterface;
 use Ghostwriter\Compliance\Event\MatrixEvent;
-use Ghostwriter\Compliance\Interface\EventListenerInterface;
+use Ghostwriter\Compliance\Interface\Event\GitHubEventInterface;
+use Ghostwriter\Compliance\Interface\Event\Listener\ListenerInterface;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\EventDispatcher\Interface\EventDispatcherInterface;
+use Throwable;
 
-final readonly class GitHubListener implements EventListenerInterface
+final readonly class GitHubListener implements ListenerInterface
 {
     public function __construct(
         private ContainerInterface $container,
-        private EventDispatcherInterface $dispatcher,
-    ) {}
+        private EventDispatcherInterface $eventDispatcher,
+    ) {
+    }
 
-    public function __invoke(GitHubEventInterface $outputEvent): void
+    /**
+     * @throws Throwable
+     */
+    public function __invoke(GitHubEventInterface $gitHubEvent): void
     {
-        $this->dispatcher->dispatch($this->container->build(MatrixEvent::class));
+        $this->eventDispatcher->dispatch($this->container->build(MatrixEvent::class));
     }
 }
