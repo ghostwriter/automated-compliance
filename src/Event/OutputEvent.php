6fd0875cfba7fb7d3c6a760ec50918ecc488c244
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:27 -0500

    Update OutputEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/OutputEvent.php
+++ src/Event/OutputEvent.php
@@ -5,20 +5,17 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Event;
 
 use Ghostwriter\Container\Container;
-use Ghostwriter\EventDispatcher\Interface\EventDispatcherInterface;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Style\StyleInterface;
 use Throwable;
 
 final readonly class OutputEvent extends AbstractEvent
 {
     public function __construct(
         private array|string $message,
-        protected EventDispatcherInterface $dispatcher,
         protected InputInterface $input,
-        protected SymfonyStyle $symfonyStyle
+        protected StyleInterface $style
     ) {
-        parent::__construct($dispatcher, $input, $symfonyStyle);
     }
 
     public function getMessage(): array|string
@@ -31,8 +28,6 @@ final readonly class OutputEvent extends AbstractEvent
      */
     public static function new(array|string $message): self
     {
-        $container = Container::getInstance();
-
-        return $container->build(self::class, [$message]);
+        return Container::getInstance()->build(self::class, [$message]);
     }
 }
