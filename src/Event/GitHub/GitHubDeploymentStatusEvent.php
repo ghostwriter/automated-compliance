9fe4aee14505cfe9776c21b10cf5f1ac1e478dfb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-16 21:26:28 -0500

    Update GitHubDeploymentStatusEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/GitHub/GitHubDeploymentStatusEvent.php
+++ src/Event/GitHub/GitHubDeploymentStatusEvent.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Event\GitHub;
 
-use Ghostwriter\Compliance\Event\GitHubEventInterface;
+use Ghostwriter\Compliance\Interface\Event\GitHubEventInterface;
+use Override;
 
 /**
  * @implements GitHubEventInterface<bool>
  */
-final class GitHubDeploymentStatusEvent implements GitHubEventInterface
+final readonly class GitHubDeploymentStatusEvent implements GitHubEventInterface
 {
     public function __construct(
         private string $content
-    ) {}
+    ) {
+    }
 
+    #[Override]
     public function payload(): string
     {
         return $this->content;
