c0f2e5b532739218eeb664c48dfe76bf6b7db701
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 10:04:26 -0500

    Add GitHubListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Event/Listener/GitHubListener.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Event\Listener;
+
+use Ghostwriter\Compliance\Event\MatrixEvent;
+use Ghostwriter\Compliance\Interface\Event\GitHubEventInterface;
+use Ghostwriter\Compliance\Interface\Event\Listener\ListenerInterface;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\EventDispatcher\Interface\EventDispatcherInterface;
+use Throwable;
+
+final readonly class GitHubListener implements ListenerInterface
+{
+    public function __construct(
+        private ContainerInterface $container,
+        private EventDispatcherInterface $eventDispatcher,
+    ) {
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function __invoke(GitHubEventInterface $gitHubEvent): void
+    {
+        $this->eventDispatcher->dispatch($this->container->build(MatrixEvent::class));
+    }
+}
