e54c1431c119405ba2cca569a11f2c19d1c97203
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-21 23:05:19 -0500

    Update src/Enum/ComposerStrategy.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Enum/ComposerStrategy.php
+++ src/Enum/ComposerStrategy.php
@@ -10,13 +10,26 @@ enum ComposerStrategy: string
     case LOCKED = 'locked'; // composer install
     case LOWEST = 'lowest'; // composer update --prefer-lowest --prefer-stable
 
+    public function isLatest(): bool
+    {
+        // Installing latest supported dependencies via Composer
+        return $this === self::LATEST;
+    }
+
+    public function isLocked(): bool
+    {
+        // Installing locked dependencies as specified in lockfile via Composer
+        return $this === self::LOCKED;
+    }
+
+    public function isLowest(): bool
+    {
+        // Installing lowest supported dependencies via Composer
+        return $this === self::LOWEST;
+    }
+
     public function toString(): string
     {
         return $this->value;
-    }
-
-    public static function isExperimental(self $composerStrategy): bool
-    {
-        return $composerStrategy === self::LOWEST;
     }
 }
