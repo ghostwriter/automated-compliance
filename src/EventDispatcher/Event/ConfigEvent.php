41c3c35716733ad8ade66a3b374f0b02d35c3f5d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-21 23:05:20 -0500

    Move src/Event/ConfigEvent.php to src/EventDispatcher/Event/ConfigEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Event/ConfigEvent.php
+++ src/EventDispatcher/Event/ConfigEvent.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Compliance\Event;
+namespace Ghostwriter\Compliance\EventDispatcher\Event;
 
 final readonly class ConfigEvent extends AbstractEvent
 {
