66d73edd8a73c4828697d0a6b6df1c47671a939f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-21 23:05:22 -0500

    Move src/Event/Listener/OutputListener.php to src/EventDispatcher/Listener/OutputListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Listener/OutputListener.php
+++ src/EventDispatcher/Listener/OutputListener.php
@@ -2,10 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Compliance\Event\Listener;
+namespace Ghostwriter\Compliance\EventDispatcher\Listener;
 
-use Ghostwriter\Compliance\Event\OutputEvent;
-use Ghostwriter\Compliance\Interface\Event\Listener\ListenerInterface;
+use Ghostwriter\Compliance\EventDispatcher\Event\OutputEvent;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 final readonly class OutputListener implements ListenerInterface
