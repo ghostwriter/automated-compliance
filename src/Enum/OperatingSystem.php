5bbefea59313a69e47568a0fefcad2c6e94c9d2a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-07 05:46:50 -0500

    Update OperatingSystem.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Enum/OperatingSystem.php
+++ src/Enum/OperatingSystem.php
@@ -6,14 +6,14 @@ namespace Ghostwriter\Compliance\Enum;
 
 enum OperatingSystem: string
 {
-    case MACOS = 'macos';
+    // case MACOS = 'macos';
     case UBUNTU = 'ubuntu';
     case WINDOWS = 'windows';
 
-    public function isMacos(): bool
-    {
-        return $this === self::MACOS;
-    }
+    // public function isMacos(): bool
+    // {
+    //     return $this === self::MACOS;
+    // }
 
     public function isUbuntu(): bool
     {
