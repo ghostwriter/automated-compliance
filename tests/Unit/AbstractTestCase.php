6b1d1cddbedf9ebdb27dfb9d19c4fa006b360118
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-04 10:38:48 -0600

    Update AbstractTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/AbstractTestCase.php
+++ tests/Unit/AbstractTestCase.php
@@ -4,14 +4,43 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use Ghostwriter\Compliance\Container\ServiceProvider;
+use Ghostwriter\Compliance\Value\Composer\Composer;
+use Ghostwriter\Container\Container;
+use Ghostwriter\Container\Interface\ContainerInterface;
 use Override;
 use PHPUnit\Framework\TestCase;
+use Throwable;
 
 abstract class AbstractTestCase extends TestCase
 {
+    public Composer $composer;
+
+    public ContainerInterface $container;
+
+    /**
+     * @throws Throwable
+     */
     #[Override]
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->container = Container::getInstance();
+
+        $this->container->provide(ServiceProvider::class);
+
+        $this->composer = $this->container->get(Composer::class);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    #[Override]
+    protected function tearDown(): void
+    {
+        $this->container->purge();
+
+        parent::tearDown();
     }
 }
