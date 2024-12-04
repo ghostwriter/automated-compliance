d2e7fefee0d0308a40b9f2dd090006d8556b1c9c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-04 10:38:48 -0600

    Update ComposerTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Service/ComposerTest.php
+++ tests/Unit/Service/ComposerTest.php
@@ -4,14 +4,23 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Service;
 
+use Ghostwriter\Compliance\Container\ServiceProvider;
 use Ghostwriter\Compliance\Value\Composer\Composer;
+use Ghostwriter\Compliance\Value\Composer\Json\ComposerJsonReader;
+use Ghostwriter\Compliance\Value\Composer\Lock\ComposerLockReader;
+use Ghostwriter\Compliance\Value\EnvironmentVariables;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use Tests\Unit\AbstractTestCase;
 use Throwable;
 
 use const DIRECTORY_SEPARATOR;
 
 #[CoversClass(Composer::class)]
+#[UsesClass(ComposerJsonReader::class)]
+#[UsesClass(ComposerLockReader::class)]
+#[UsesClass(EnvironmentVariables::class)]
+#[UsesClass(ServiceProvider::class)]
 final class ComposerTest extends AbstractTestCase
 {
     /**
@@ -21,7 +30,7 @@ final class ComposerTest extends AbstractTestCase
     {
         $root = \getcwd();
 
-        self::assertSame((new Composer())->getJsonFilePath($root), $root . DIRECTORY_SEPARATOR . 'composer.json');
+        self::assertSame($this->composer->getJsonFilePath($root), $root . DIRECTORY_SEPARATOR . 'composer.json');
     }
 
     /**
@@ -31,6 +40,6 @@ final class ComposerTest extends AbstractTestCase
     {
         $root = \getcwd();
 
-        self::assertSame((new Composer())->getLockFilePath($root), $root . DIRECTORY_SEPARATOR . 'composer.lock');
+        self::assertSame($this->composer->getLockFilePath($root), $root . DIRECTORY_SEPARATOR . 'composer.lock');
     }
 }
