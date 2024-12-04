590a00415297a5520f9a681c5432866fa397f46f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-04 09:34:12 -0600

    Add ComposerJsonFile.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Composer/Json/ComposerJsonFile.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Value\Composer\Json;
+
+use Ghostwriter\Compliance\Value\Composer\ComposerFile;
+
+interface ComposerJsonFile extends ComposerFile
+{
+}
