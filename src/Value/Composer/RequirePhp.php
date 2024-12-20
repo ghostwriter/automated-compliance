f4baaf9d22f35e2352f719184a86329d9383dfed
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:43 -0600

    Update RequirePhp.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Composer/RequirePhp.php
+++ src/Value/Composer/RequirePhp.php
@@ -6,21 +6,26 @@ namespace Ghostwriter\Compliance\Value\Composer;
 
 use Override;
 
+use function mb_trim;
+
 final readonly class RequirePhp implements PhpVersionConstraintInterface
 {
     public function __construct(
         private string $version,
     ) {
+        if (mb_trim($version) === '') {
+            throw new InvalidArgumentException('PHP version cannot be empty');
+        }
+    }
+
+    public static function new(string $phpVersion): self
+    {
+        return new self($phpVersion);
     }
 
     #[Override]
     public function getVersion(): string
     {
         return $this->version;
-    }
-
-    public static function new(string $phpVersion): self
-    {
-        return new self($phpVersion);
     }
 }
