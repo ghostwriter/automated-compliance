b274b157f2e1284ce8a599412a6c4804fee6f51f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:30 -0600

    Update GitHubPullRequestCommentEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Event/GitHub/GitHubPullRequestCommentEvent.php
+++ src/EventDispatcher/Event/GitHub/GitHubPullRequestCommentEvent.php
@@ -14,8 +14,7 @@ final readonly class GitHubPullRequestCommentEvent implements GitHubEventInterfa
 {
     public function __construct(
         private string $content
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function payload(): string
