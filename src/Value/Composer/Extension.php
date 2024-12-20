6da7cc0485db0f7c6675403344b66b0b09f5c5ef
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:39 -0600

    Update Extension.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Composer/Extension.php
+++ src/Value/Composer/Extension.php
@@ -7,18 +7,24 @@ namespace Ghostwriter\Compliance\Value\Composer;
 use Ghostwriter\Compliance\Interface\Composer\DependencyInterface;
 use Override;
 
+use function mb_substr;
+
 final readonly class Extension implements DependencyInterface
 {
     public function __construct(
         private DependencyName $dependencyName,
         private DependencyVersion $dependencyVersion
-    ) {
+    ) {}
+
+    public static function new(DependencyName $dependencyName, DependencyVersion $dependencyVersion): self
+    {
+        return new self($dependencyName, $dependencyVersion);
     }
 
     #[Override]
     public function __toString(): string
     {
-        return \mb_substr((string) $this->dependencyName, 4);
+        return mb_substr((string) $this->dependencyName, 4);
     }
 
     #[Override]
@@ -39,10 +45,5 @@ final readonly class Extension implements DependencyInterface
     public function version(): DependencyVersion
     {
         return $this->dependencyVersion;
-    }
-
-    public static function new(DependencyName $dependencyName, DependencyVersion $dependencyVersion): self
-    {
-        return new self($dependencyName, $dependencyVersion);
     }
 }
