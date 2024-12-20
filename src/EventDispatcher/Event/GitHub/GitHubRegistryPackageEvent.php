0bdfe922399d9e875a01147ad85a4fbd245556ab
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:30 -0600

    Update GitHubRegistryPackageEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Event/GitHub/GitHubRegistryPackageEvent.php
+++ src/EventDispatcher/Event/GitHub/GitHubRegistryPackageEvent.php
@@ -14,8 +14,7 @@ final readonly class GitHubRegistryPackageEvent implements GitHubEventInterface
 {
     public function __construct(
         private string $content
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function payload(): string
