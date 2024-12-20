981dba0eb43f04cab3df3dffe351e4bdf889e737
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:28 -0600

    Update GitHubIssueCommentEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Event/GitHub/GitHubIssueCommentEvent.php
+++ src/EventDispatcher/Event/GitHub/GitHubIssueCommentEvent.php
@@ -14,8 +14,7 @@ final readonly class GitHubIssueCommentEvent implements GitHubEventInterface
 {
     public function __construct(
         private string $content
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function payload(): string
