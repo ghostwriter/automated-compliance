b2d9b73fdfa10b36ffff82a899fe4f1897af6b45
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:31 -0600

    Update GitHubReleaseEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Event/GitHub/GitHubReleaseEvent.php
+++ src/EventDispatcher/Event/GitHub/GitHubReleaseEvent.php
@@ -14,8 +14,7 @@ final readonly class GitHubReleaseEvent implements GitHubEventInterface
 {
     public function __construct(
         private string $content
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function payload(): string
