021d52547559fb60be5f6ddcf9bd8937b4ea4267
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:31 -0600

    Update GitHubScheduleEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Event/GitHub/GitHubScheduleEvent.php
+++ src/EventDispatcher/Event/GitHub/GitHubScheduleEvent.php
@@ -14,8 +14,7 @@ final readonly class GitHubScheduleEvent implements GitHubEventInterface
 {
     public function __construct(
         private string $content
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function payload(): string
