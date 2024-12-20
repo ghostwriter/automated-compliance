a8ec2c5fc4f802c396219ecc0acee2b02cf9887b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:28 -0600

    Update GitHubForkEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/EventDispatcher/Event/GitHub/GitHubForkEvent.php
+++ src/EventDispatcher/Event/GitHub/GitHubForkEvent.php
@@ -14,8 +14,7 @@ final readonly class GitHubForkEvent implements GitHubEventInterface
 {
     public function __construct(
         private string $content
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function payload(): string
