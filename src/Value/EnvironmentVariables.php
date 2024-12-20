9a5b1a8d56b95b5b58e82bb48a77bb232e6017b9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-20 10:58:43 -0600

    Update EnvironmentVariables.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/EnvironmentVariables.php
+++ src/Value/EnvironmentVariables.php
@@ -7,28 +7,45 @@ namespace Ghostwriter\Compliance\Value;
 use Ghostwriter\Compliance\Exception\VariableNotFoundException;
 use Throwable;
 
+use function array_key_exists;
+use function getenv;
+
 final class EnvironmentVariables
 {
     private function __construct(
         private array $variables = [],
-    ) {
+    ) {}
+
+    public static function new(): self
+    {
+        $variables = [];
+
+        foreach ($_ENV as $name => $value) {
+            $variables[$name] = (string) $value;
+        }
+
+        foreach (getenv() ?: [] as $name => $value) {
+            $variables[$name] = $value;
+        }
+
+        return new self($variables);
     }
 
     /**
      * @throws Throwable
      */
-    public function get(string $name, null|string $default = null): string
+    public function get(string $name, ?string $default = null): string
     {
         return match (true) {
-            \array_key_exists($name, $this->variables) => $this->variables[$name],
-            $default !== null => $default,
+            array_key_exists($name, $this->variables) => $this->variables[$name],
+            null !== $default => $default,
             default => throw new VariableNotFoundException($name),
         };
     }
 
     public function has(string $name): bool
     {
-        return \array_key_exists($name, $this->variables);
+        return array_key_exists($name, $this->variables);
     }
 
     public function set(string $name, string $value): void
@@ -40,20 +57,5 @@ final class EnvironmentVariables
         $_SERVER[$name] = $value;
 
         // putenv($name . '=' . $value);
-    }
-
-    public static function new(): self
-    {
-        $variables = [];
-
-        foreach ($_ENV as $name => $value) {
-            $variables[$name] = (string) $value;
-        }
-
-        foreach (\getenv() ?: [] as $name => $value) {
-            $variables[$name] = $value;
-        }
-
-        return new self($variables);
     }
 }
